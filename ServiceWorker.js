91f117e46edcb42a534602f01a858f3fc03612b8
Kimiya Poozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2024-12-25 20:48:32 +0330

    new test on opt

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "KimiaPoozesh-Portfolio-0.1";
 const contentToCache = [
-    "Build/dbf2241f913827ad9086b9e9f4204713.loader.js",
-    "Build/c4de552d015870d179c511769d278776.framework.js",
-    "Build/caefc4b135ffa4fa4d67c9ed4203163e.data",
-    "Build/5046bd0a3b215e8a58a37ddd6f1e24e8.wasm",
+    "Build/Portfolio.loader.js",
+    "Build/Portfolio.framework.js",
+    "Build/Portfolio.data",
+    "Build/Portfolio.wasm",
     "TemplateData/style.css"
 
 ];
